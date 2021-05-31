ddaf3d5d2c54c1ced174b5c0e4ebcbc07330a232
Misha Zamkevich <uwwcom@gmail.com>
2021-05-31 12:32:31 +1000

    gosuch page

--- src/routes/gosuch/index.svelte
+++ src/routes/gosuch/index.svelte
@@ -3,60 +3,18 @@
 </script>
 
 <script>
-import PhotoTim from '../../static/team/tim.png?w=120;240&format=webp;avif;jpg&srcset'
-import PhotoKostia from '../../static/team/kostia.png?w=120;240&format=webp;avif;jpg&srcset'
-import PhotoLida from '../../static/team/lida.png?w=120;240&format=webp;avif;jpg&srcset'
-import PhotoNikita from '../../static/team/nikita.png?w=120;240&format=webp;avif;jpg&srcset'
-import PhotoMisha from '../../static/team/misha.png?w=120;240&format=webp;avif;jpg&srcset'
-import PhotoNatasha from '../../static/team/natasha.png?w=120;240&format=webp;avif;jpg&srcset'
-import PhotoAlena from '../../static/team/alena.png?w=120;240&format=webp;avif;jpg&srcset'
+import PhotoKostia from '../../../static/team/kostia.png?w=120;240&format=webp;avif;jpg&srcset'
+import PhotoLida from '../../../static/team/lida.png?w=120;240&format=webp;avif;jpg&srcset'
+import PhotoNikita from '../../../static/team/nikita.png?w=120;240&format=webp;avif;jpg&srcset'
 </script>
 
 <svelte:head>
-  <title>Zakutok.media</title>
+  <title>Zakutok.media | Gosuch</title>
 </svelte:head>
 
 
 
 <template lang='pug'>
-section#shmarketing.podcast
-  h1.logotype.smarketing Шмаркетинг
-  .description_block
-    p.p.text
-      | Чудо-подк&shy;аст о творче&shy;стве, рекла&shy;ме, маркет&shy;инге, дизай&shy;не, предпри&shy;ни&shy;матель&shy;стве на краю земли во Влад&shy;ивост&shy;оке
-
-    .host-block
-      .host
-        img(
-          alt='Тимофей, соведущий'
-          srcset!='{PhotoTim}' type="image/webp"
-          width='120px'
-          height='120px'
-          )
-        a(href='https://www.instagram.com/timbeloglazov/' target='_blank') Тимофей
-          span соведущий
-      .host
-        img(
-          alt='Костя, ведущий'
-          srcset!='{PhotoKostia}' type="image/webp"
-          width='120px'
-          height='120px'
-          )
-        a(href='https://www.instagram.com/koko3kote/' target='_blank') Костя
-          span Ведущий
-
-  h2.h3 Послушать
-  .links
-    a(href='https://podcasts.apple.com/podcast/id1549369792?mt=2' target='_blank') Apple Podcasts
-    a(href='https://music.yandex.ru/album/13523721' target='_blank') Я.Музыка
-    a(href='https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy80ODM0NGVkMC9wb2RjYXN0L3Jzcw' target='_blank') Google Podcasts
-    a(href='https://overcast.fm/itunes1549369792' target='_blank') Overcast
-    a(href='https://pca.st/nc6mbk5o' target='_blank') Pocket Casts
-    a(href='https://open.spotify.com/show/0LDb8JioFYVGlcJivDQ2uP' target='_blank') Spotify
-    a(href='https://castbox.fm/vic/1549369792' target='_blank') Castbox
-    a(href='https://castro.fm/itunes/1549369792' target='_blank') Castro
-    a(href='https://anchor.fm/s/48344ed0/podcast/rss' target='_blank') RSS
-
 section#gosuch.podcast
   h1.logotype.gosuch Gosuch
   .description_block
@@ -102,115 +60,6 @@ section#gosuch.podcast
     a(href='https://radiopublic.com/gosuch-6rj7RK' target='_blank') Radio Public
     a(href='https://zakutokmedia.ru/gosuch/rss' target='_blank') RSS
 
-section#5cents.podcast
-  h1.logotype.five-cents Пятицентовая поэзия
-  .description_block
-    p.p.text
-      | Подкаст о поэзии, прозе и поэтах Вла&shy;ди&shy;во&shy;стока. Поэтом може&shy;шь ты не быть, а, в общем-то, и не обязан
-
-
-    .host-block
-      .host
-        img(
-          alt='Костя, ведущий'
-          srcset!='{PhotoKostia}' type="image/webp"
-          width='120px'
-          height='120px'
-          )
-        a(href='https://www.instagram.com/koko3kote/' target='_blank') Костя
-          span Ведущий
-
-  h2.h3 Послушать
-  .links
-    a(href='https://podcasts.apple.com/podcast/id1566497764' target='_blank') Apple Podcasts
-    a(href='https://music.yandex.ru/album/15021185' target='_blank') Я.Музыка
-    a(href='https://overcast.fm/itunes1566497764' target='_blank') Overcast
-    a(href='https://castbox.fm/vic/1566497764' target='_blank') Castbox
-    a(href='https://pca.st/itunes/1566497764' target='_blank') Pocket Casts
-    a(href='https://open.spotify.com/show/6MWMFax8t2BIm16RD9l7NI' target='_blank') Spotify
-    a(href='https://castro.fm/itunes/1566497764' target='_blank') Castro
-    a(href='https://www.breaker.audio/shows?feed_url=https%3A%2F%2Fanchor.fm%2Fs%2F5573ac44%2Fpodcast%2Frss' target='_blank') Breaker
-    a(href='https://radiopublic.com/https%3A%2F%2Fanchor.fm%2Fs%2F5573ac44%2Fpodcast%2Frss' target='_blank') Radio Public
-    a(href='https://anchor.fm/s/5573ac44/podcast/rss' target='_blank') RSS
-
-section.about
-  h1.h2 Соуча&shy;стни&shy;ки
-
-  .host-block.host-block---full
-    .host
-      img(
-        alt='Костя, ведущий'
-        srcset!='{PhotoKostia}' type="image/webp"
-        width='120px'
-        height='120px'
-        )
-      a(href='https://www.instagram.com/koko3kote/' target='_blank') Костя
-        span Ведущий
-
-    .host
-      img(
-        alt='Лида, соведущая'
-        srcset!='{PhotoLida}' type="image/webp"
-        width='120px'
-        height='120px'
-        )
-      a(href='https://www.instagram.com/pihora/' target='_blank') Лида
-        span Соведущая
-
-    .host
-      img(
-        alt='Никита, соведущий'
-        srcset!='{PhotoNikita}' type="image/webp"
-        width='120px'
-        height='120px'
-        )
-      a(href='https://www.instagram.com/nn_sergeevich/' target='_blank') Никита
-        span Соведущий
-
-    .host
-      img(
-        alt='Тимофей, соведущий'
-        srcset!='{PhotoTim}' type="image/webp"
-        width='120px'
-        height='120px'
-        )
-      a(href='https://www.instagram.com/timbeloglazov/' target='_blank') Тимофей
-        span Соведущий
-
-    .host
-      img(
-        alt='Миша, делает красиво'
-        srcset!='{PhotoMisha}' type="image/webp"
-        width='120px'
-        height='120px'
-        )
-      a(href='https://www.instagram.com/misharodshtein/' target='_blank') Миша
-        span Делает красиво
-
-    .host
-      img(
-        alt='Алёна, даёт денег'
-        srcset!='{PhotoAlena}' type="image/webp"
-        width='120px'
-        height='120px'
-        )
-      a(href='https://www.instagram.com/_tirle_/' target='_blank') Алёна
-        span Даёт денег
-
-    .host
-      img(
-        alt='Наташа, Продвигает'
-        srcset!='{PhotoNatasha}' type="image/webp"
-        width='120px'
-        height='120px'
-        )
-      a(href='https://www.instagram.com/ladutya/' target='_blank') Наташа
-        span Продвигает
-
-  p.text
-    | Подкастерск&shy;ий за&shy;кут&shy;ок нера&shy;внод&shy;ушн&shy;ых реб&shy;ят из Вла&shy;дивос&shy;тока. Бо&shy;лта&shy;ем и пиш&shy;ем в 410 кабине&shy;те на Посье&shy;тс&shy;кой, 45. Zakutok.media — неко&shy;ммерче&shy;ский прое&shy;кт гру&shy;ппы компа&shy;ний
-    |
-    a(href='http://kokovikhin.digital/' target='_blank') Kokovikhin.Digital
 </template>
 
 
@@ -512,15 +361,6 @@ section.about
     @media ( width > 1280px ){
       font-size: 60px;
     }
-  }
-}
-
-.about {
-  .h2 {
-    margin-bottom: 40px;
-  }
-  .host-block {
-    margin-bottom: 90px;
   }
 }
 </style>
