8934e6d397bbff18ad5c772434c73c9c4f0aaed9
Misha Zamkevich <uwwcom@gmail.com>
2021-06-03 19:06:41 +1000

    test

--- src/lib/metrica.svelte
+++ src/lib/metrica.svelte
@@ -20,7 +20,8 @@
   })
 
   function init() {
-    getScript().onload = initCounter
+    setMetrica()
+    // getScript().onload = initCounter
   }
 
   function getScript() {
@@ -31,6 +32,36 @@
 
     document.body.appendChild(script)
     return script
+  }
+
+  function setMetrica() {
+
+    window.ym = window.ym || function () {
+      (window.ym.a = window.ym.a || []).push(arguments)
+    };
+
+    window.ym.l = 1 * new Date();
+
+
+    let firstScript = document.getElementsByTagName("script")[0];
+    let src = "https://mc.yandex.ru/metrika/tag.js";
+
+
+    let script = document.createElement("script");
+    script.async = 1
+    script.src = src
+
+
+    firstScript.parentNode.insertBefore(script, FirstScript)
+
+    ym(id, "init", {
+      defer: true,
+      clickmap,
+      trackLinks,
+      accurateTrackBounce,
+      triggerEvent,
+      webvisor
+    });
   }
 
   function initCounter() {
